6b9afe2f3f146af9f668888c0881b41937d86e0a
xaoccc <xaocccc@gmail.com>
2026-04-07 14:45:24 +0300

    my first Android app (in Kotlin) - first use of onStart, onDestroy and intentions

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/AnotherActivity.kt
@@ -0,0 +1,55 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.myfirstapp.ui.theme.MyFirstAppTheme
+
+class AnotherActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyFirstAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting("Gosho","vodka", Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        enableEdgeToEdge()
+        setContent {
+            MyFirstAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Shutdown(Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, drink: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello, I am $name! I like $drink",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Shutdown(modifier:Modifier = Modifier) {
+    Text(
+        text = "Android is now shutting down...",
+        modifier = modifier
+    )
+}
---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.myfirstapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+import com.example.myfirstapp.ui.theme.MyFirstAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyFirstAppTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = {
+//                        Here we trigger the second activity by clicking the button
+                        Intent(applicationContext, AnotherActivity::class.java).also {
+                            startActivity(it)
+                        }
+                    }) {
+                        Text(text = "Submit")
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+
+
+
+
